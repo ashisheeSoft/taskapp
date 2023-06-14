3b6d8da54089caebb309224e8122b24250ee2e5e
ashisheeSoft <ashishsharmaen1014@gmail.com>
2023-06-14 23:05:00 +0530

    initail commit

---
+++ task-auth/src/main/java/com/ashish/taskauth/TaskAuthApplication.java
@@ -0,0 +1,13 @@
+package com.ashish.taskauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskAuthApplication.class, args);
+	}
+
+}
---
+++ task-auth/src/test/java/com/ashish/taskauth/TaskAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashish.taskauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
